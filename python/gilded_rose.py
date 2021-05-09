2320466f8c02c93c9476a4186021d0bca9d0cca3
vgrillot <grivin@gmail.com>
2021-05-09 17:40:21 +0200

    extract update logic to Item class

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+import abc
+
 
 class GildedRose(object):
 
@@ -7,36 +9,10 @@ class GildedRose(object):
 
     def update_quality(self):
         for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
-            else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
-                else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
+            item.update_quality()
 
 
-class Item:
+class BaseItem:
     def __init__(self, name, sell_in, quality):
         self.name = name
         self.sell_in = sell_in
@@ -44,3 +20,38 @@ class Item:
 
     def __repr__(self):
         return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
+
+    @abc.abstractmethod
+    def update_quality(self):
+        pass
+
+
+class Item(BaseItem):
+    def update_quality(self):
+        if self.name != "Aged Brie" and self.name != "Backstage passes to a TAFKAL80ETC concert":
+            if self.quality > 0:
+                if self.name != "Sulfuras, Hand of Ragnaros":
+                    self.quality = self.quality - 1
+        else:
+            if self.quality < 50:
+                self.quality = self.quality + 1
+                if self.name == "Backstage passes to a TAFKAL80ETC concert":
+                    if self.sell_in < 11:
+                        if self.quality < 50:
+                            self.quality = self.quality + 1
+                    if self.sell_in < 6:
+                        if self.quality < 50:
+                            self.quality = self.quality + 1
+        if self.name != "Sulfuras, Hand of Ragnaros":
+            self.sell_in = self.sell_in - 1
+        if self.sell_in < 0:
+            if self.name != "Aged Brie":
+                if self.name != "Backstage passes to a TAFKAL80ETC concert":
+                    if self.quality > 0:
+                        if self.name != "Sulfuras, Hand of Ragnaros":
+                            self.quality = self.quality - 1
+                else:
+                    self.quality = self.quality - self.quality
+            else:
+                if self.quality < 50:
+                    self.quality = self.quality + 1
